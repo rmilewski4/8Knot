2cd7e769135451e67eefd1184bd30751e68daaae
Matt <thequantumblink@gmail.com>
2023-12-10 11:55:27 -0600

    cleaned up health page and properly added contributor count page

--- 8Knot/pages/community_health/community_health.py
+++ 8Knot/pages/community_health/community_health.py
@@ -4,12 +4,10 @@ import dash_bootstrap_components as dbc
 import warnings
 
 # import visualization cards
-from .visualizations.project_velocity import gc_project_velocity
 from .visualizations.change_request_review_count import change_request_review_count
-from .visualizations.bus_factor             import gc_bus_factor_pie
-from .visualizations.change_req_close_ratio import gc_change_req_closure_ratio
-from .visualizations.release_freq import gc_release_freq
+
 from .visualizations.issues_closed import gc_issues_closed
+from .visualizations.contributor_count import gc_contributor_count
 
 warnings.filterwarnings("ignore")
 
@@ -27,7 +25,7 @@ layout = dbc.Container(
         ),
         dbc.Row(
             [
-                #dbc.Col(gc_bus_factor_pie, width=6),
+                dbc.Col(gc_contributor_count, width=6),
                 #dbc.Col(change_request_review_count, width=6),
             ],
             align="center",
